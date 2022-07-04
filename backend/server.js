548085896940b0e9e39bfaf9e78e5bc1c4bf61b0
Vishnu Sivan <codemaker2015@gmail.com>
2022-07-04 13:50:05 +0530

    corrections

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,16 @@
 const express = require('express')
-const path = require('path')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 
 // Connecting with mongo db
 mongoose
-  .connect('mongodb://127.0.0.1:27017/mydatabase')
+  .connect('mongodb://127.0.0.1:27017/employeedb')
   .then((x) => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+    console.log(`Connected to ${x.connections[0].name}`)
   })
   .catch((err) => {
-    console.error('Error connecting to mongo', err.reason)
+    console.error('Error occurs while connecting to the database, ', err.reason)
   })
 
 // Setting up port with express js
@@ -24,19 +23,17 @@ app.use(
   }),
 )
 app.use(cors())
-// app.use(express.static(path.join(__dirname, 'dist/mean-stack-crud-app')))
-// app.use('/', express.static(path.join(__dirname, 'dist/mean-stack-crud-app')))
-app.use('/', employeeRoute)
+app.use('/api', employeeRoute)
 
 // Create port
 const port = process.env.PORT || 4000
 const server = app.listen(port, () => {
-  console.log('Connected to port ' + port)
+  console.log('Server is listening to port ' + port)
 })
 
 // Find 404 and hand over to error handler
 app.use((req, res, next) => {
-  next(createError(404))
+  next(console.error("Oops! error occurs"))
 })
 
 // error handler
